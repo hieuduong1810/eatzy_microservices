92ec7ce8644c2c6d9a2c661f62d7bb10785e018f
Hieuduong <hieuduong181005@gmail.com>
2026-04-29 21:17:09 +0700

    feat: add one-indexed parameters for pageable configuration across all services

--- eatzy-cart-service/src/main/java/com/eatzy/cart/EatzyCartServiceApplication.java
+++ eatzy-cart-service/src/main/java/com/eatzy/cart/EatzyCartServiceApplication.java
@@ -1,12 +1,14 @@
 package com.eatzy.cart;
 
+import com.eatzy.common.config.FeignConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import com.eatzy.common.config.FeignConfig;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.eatzy.cart", "com.eatzy.common" })
 @EnableDiscoveryClient
 @EnableFeignClients(defaultConfiguration = FeignConfig.class)
 public class EatzyCartServiceApplication {
